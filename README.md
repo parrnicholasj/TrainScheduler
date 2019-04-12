bc5444fda32a19ea29e570643ae4d2979e79b98b
Nicholas Parr <parrnicholasj@gmail.com>
2019-04-11 21:41:19 -0400

    things went wrong but the database works

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TrainScheduler
+supposed to allow user to upload train info to database
+then tell when things are happening
+
+unfortunately I could not figure my way around moment.js so this is currently broken
+the database works at least
+
+##tech used
+html
+javascript
+bootstrap
+moment.js
+firebase database
--- schedule.js
+++ schedule.js
@@ -1,14 +1,16 @@
 var config = {
-  apiKey: "AIzaSyCqXd6ALMEJXVXf7-Ztu1yzItimSAYALWU",
-  authDomain: "trainhomework-ec91a.firebaseapp.com",
-  databaseURL: "https://trainhomework-ec91a.firebaseio.com",
-  projectId: "trainhomework-ec91a",
-  storageBucket: "trainhomework-ec91a.appspot.com",
-  messagingSenderId: "313669188301"
+  apiKey: "AIzaSyBnlPXtVRfBXc2DPmQ8D_3oerITukAeCoY",
+  authDomain: "trainhomework-2d53f.firebaseapp.com",
+  databaseURL: "https://trainhomework-2d53f.firebaseio.com",
+  projectId: "trainhomework-2d53f",
+  storageBucket: "trainhomework-2d53f.appspot.com",
+  messagingSenderId: "919231237041"
 };
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+var timeTill;
 
 $("#addTrain").on("click", function (event)
 {
@@ -34,9 +36,71 @@ $("#addTrain").on("click", function (event)
 
 });
 
-$(".tbody").on("value", function(){
+database.ref().on("child_added", function (snapshot)
+{
 
-  console.log("value");
+  console.log(snapshot.val().name);
+
+  var $tr = $("<tr>").append(
+  $("<td>").text(snapshot.val().name),
+  $("<td>").text(snapshot.val().dest),
+  $("<td>").text(snapshot.val().arrival),
+  $("<td>").text(snapshot.val().freq),
+  $("<td>").text(getNextArrival(snapshot.val().freq, snapshot.val().arrival)),
+  $("<td>").text(getremaining(snapshot.val().freq))
+
+  )
+  
+  $(".tbody").append($tr);
 
 })
+
+function getNextArrival(frequency, first)
+{
+  //first check if it is later than when the first train will arrive
+  //apparently now is always after whatever time i put in
+  var now = moment().format("HH:mm");
+
+  now = parseInt(now);
+  console.log(now);
+
+  var intFirst = parseInt(first)
+
+  if (now > intFirst)
+  {
+
+    var firstMom = moment(first).format("HH:mm");
+    moment(firstMom).add(frequency, "minutes").format("HH:mm");
+    timeTill = firstMom;
+    return firstMom;
+
+  } else if (now == intFirst)
+  {
+    var firstMom = moment(first).format("HH:mm");
+    moment(firstMom).add(frequency, "minutes").format("HH:mm");
+    timeTill = firstMom;
+    return firstMom;
+  }else {//it will do for now
+console.log("else");
+    now = moment().format("HH:mm");
+    moment(now).add(frequency, "minutes").format("HH:mm");
+    timeTill = now;
+    return now;
+
+  }
+
+}
+
+function getremaining(frequency){
+
+  console.log(frequency);
+  console.log(timeTill);
+  console.log(moment(timeTill).subtract(frequency, "HH:mm"));
+  return moment(timeTill).subtract(frequency, "HH:mm");
+
+}
+
+var test = moment("23:10", "HH:mm");
+console.log(getNextArrival(20, test));
+
 
